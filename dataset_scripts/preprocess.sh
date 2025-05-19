7f9b3b01240282c450df55c35447925afe5dfaa6
qin1122 <qinhn040516@163.com>
2025-05-20 02:13:06 +0800

    update preprocess.sh

--- dataset_scripts/preprocess.sh
+++ dataset_scripts/preprocess.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-HR_DIR="/root/Homeworks/NLP/FLUX_SR/datasets/DIV2K_train_HR"
-LR_DIR="/root/Homeworks/NLP/FLUX_SR/datasets/DIV2K_train_LR_x4"
-PROMPT_DIR="/root/Homeworks/NLP/FLUX_SR/datasets/DIV2K_train_prompt_short"
-LR_FIX_SIZE_DIR="/root/Homeworks/NLP/FLUX_SR/datasets/DIV2K_train_LR_x4_fixed"
-HF_DATASET_DIR="/root/Homeworks/NLP/FLUX_SR/datasets/DIV2K_train_dataset_x4_withprompt"
+HR_DIR="/NASdata/dyw/FLUX-SR/datasets/DIV2K_train_HR"
+LR_DIR="/NASdata/dyw/FLUX-SR/datasets/DIV2K_train_LR_x8"
+PROMPT_DIR="/NASdata/dyw/FLUX-SR/datasets/DIV2K_train_prompt_short"
+LR_FIX_SIZE_DIR="/NASdata/dyw/FLUX-SR/datasets/DIV2K_train_LR_x8_fixed"
+HF_DATASET_DIR="/NASdata/dyw/FLUX-SR/datasets/DIV2K_train_dataset_x8_withprompt"
 
 # downsample image
-python generate_LR_image.py \
+python dataset_scripts/generate_LR_image.py \
     --input "$HR_DIR" \
     --output "$LR_DIR" \
     --output_fix_size "$LR_FIX_SIZE_DIR"
 
 # build hf dataset
-python generate_dataset.py \
+python dataset_scripts/generate_dataset.py \
     --control_dir "$LR_DIR"\
     --target_dir "$HR_DIR" \
     --caption_dir "$PROMPT_DIR" \
     --output_dir "$HF_DATASET_DIR"
+
+HR_DIR="/NASdata/dyw/FLUX-SR/datasets/DIV2K_valid_HR"
+LR_DIR="/NASdata/dyw/FLUX-SR/datasets/DIV2K_valid_LR_x8"
+LR_FIX_SIZE_DIR="/NASdata/dyw/FLUX-SR/datasets/DIV2K_valid_LR_x8_fixed"
+
+# downsample image
+python dataset_scripts/generate_LR_image.py \
+    --input "$HR_DIR" \
+    --output "$LR_DIR" \
+    --output_fix_size "$LR_FIX_SIZE_DIR"
